d0d01176e512f5a387595ef06c8ba32db568284e
Thomas AWOUNFOUET <thomas.awounfouet@gmail.com>
2026-02-09 00:39:28 +0100

    suppression uploadThing

--- app/api/uploadthing-v1/route.ts
+++
@@ -1,9 +0,0 @@
-import { createRouteHandler } from "uploadthing/next";
-import { ourFileRouter } from "./core";
-
-// Configuration pour indiquer à Next.js que cette route est dynamique
-export const dynamic = "force-dynamic";
-
-export const { GET, POST } = createRouteHandler({
-  router: ourFileRouter,
-});
